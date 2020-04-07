1ed5a6752b14b96a75d6c77f53bc125c92021c1b
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-07 11:20:11 -0500

    CSS connects

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const PORT = process.env.PORT
 
 require('./db/db')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
 	res.render('home.ejs')
 })
 
+app.get('*', (req, res) => {
+	res.render('404.ejs')
+})
 
 app.listen(PORT, () => {
 	console.log(`Running on ${PORT}`);
