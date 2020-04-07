e0559932dcba83e7876fde89f7791690dc57e34c
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-07 11:27:04 -0500

    session added

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+	res.render('auth/login.ejs')
+})
+
+
+
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,10 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
+const session = require('express-session')
 
 require('./db/db')
 
 app.use(express.static('public'))
+
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false
+}))
+
+const authController = require('./controllers/authController')
+app.use('/auth', authController)
+
 
 app.get('/', (req, res) => {
 	res.render('home.ejs')
