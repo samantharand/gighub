3026b9f2d1536591596f364aa7fab884864cc590
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-04-08 11:13:01 -0500

    band index route works

--- controllers/bandController.js
+++ controllers/bandController.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const Band = require('//models/user')
-
+const Band = require('../models/band')
+//index route
+router.get('/', async (req, res, next) => {
+  try {
+  		const foundBands = await Band.find().populate('user')
+  		console.log(foundBands)
+  		res.render('bands/index.ejs', {bands: foundBands})
+  	}catch(error){
+  		next(error)
+  	}
+  })
 
 
 
