66f58a5faa5126207c626345a7cbb22473532e73
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-04-09 14:45:59 -0500

    comment edit page renders

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Event = require("../models/event")
 const User = require('../models/user')
-
+const Comment = require('../models/comment')
 router.post('/:eventId', async (req, res, next) => {
   try {
   		const event = await Event.findById(req.params.eventId)
@@ -21,6 +21,39 @@ router.post('/:eventId', async (req, res, next) => {
   		next(error)
   	}
   })
+// router.delete("/:eventId/:commentId", async (req, res, next) => {
+//   try {
+//       await Comment.findByIdAndRemove(req.params.commentId)
+//       res.redirect(`/events/${req.params.eventId}`)
+//     }catch(error){
+//       next(error)
+//     }
+//   })
+router.get('/:eventId/:commentId/edit', async (req, res, next) => {
+  try {
+      const foundEvent = await Event.findById(req.params.eventId)
+      console.log("\n found event")
+      console.log(foundEvent)
 
+      const foundComment = foundEvent.comments.id(req.params.commentId) 
+      console.log("\n found comment")
+      console.log(foundComment)
+      res.render("comments/edit.ejs", {comment: foundComment})
+    }catch(error){
+      next(error)
+    }
+})
+// router.put('/:commentId', async (req, res, next) => {
+//   try {
 
+//       const updatedComment = await Comment.findByIdAndUpdate(
+//         req.params.commentId,
+//         req.body,
+//         {new:true}
+//         )
+//       res.send("updated comment")
+//     }catch(error){
+//       next(error)
+//     }
+//   })
 module.exports= router
