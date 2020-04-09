5d81ad75b87c18a7bd2ffb0b373d72116a81aba4
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-04-09 15:41:05 -0500

    comments delete

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -21,37 +21,45 @@ router.post('/:eventId', async (req, res, next) => {
   		next(error)
   	}
   })
-// router.delete("/:eventId/:commentId", async (req, res, next) => {
-//   try {
-//       await Comment.findByIdAndRemove(req.params.commentId)
-//       res.redirect(`/events/${req.params.eventId}`)
-//     }catch(error){
-//       next(error)
-//     }
-//   })
+router.delete("/:eventId/:commentId", async (req, res, next) => {
+  try {
+      const foundEvent = await Event.findById(req.params.eventId)
+       const commentToRemove = await foundEvent.comments.id(req.params.commentId).remove() 
+       console.log(commentToRemove)
+      foundEvent.save()
+      res.redirect(`/events/${req.params.eventId}`)
+    }catch(error){
+      next(error)
+    }
+  })
 router.get('/:eventId/:commentId/edit', async (req, res, next) => {
   try {
       const foundEvent = await Event.findById(req.params.eventId)
-      console.log("\n found event")
-      console.log(foundEvent)
+     
 
       const foundComment = foundEvent.comments.id(req.params.commentId) 
-      console.log("\n found comment")
-      console.log(foundComment)
-      res.render("comments/edit.ejs", {comment: foundComment})
+    
+      res.render("comments/edit.ejs", {event: foundEvent, comment: foundComment})
     }catch(error){
       next(error)
     }
 })
-// router.put('/:commentId', async (req, res, next) => {
+// router.put('/:eventId/:commentId', async (req, res, next) => {
 //   try {
+//         const foundEvent = await Event.findById(req.params.eventId)
+     
 
-//       const updatedComment = await Comment.findByIdAndUpdate(
-//         req.params.commentId,
-//         req.body,
-//         {new:true}
-//         )
-//       res.send("updated comment")
+//       const foundComment = foundEvent.comments.id(req.params.commentId) 
+//       console.log(`this is found comment ${foundComment}`)
+//       console.log(foundComment._id)
+//       console.log("this is req.body")
+//       console.log(req.body)
+      
+//       await Comment.findOneAndReplace({id: foundComment._id}, {text: req.body.text})
+       
+      
+//       res.redirect(`/events/${req.params.eventId}`)
+
 //     }catch(error){
 //       next(error)
 //     }
