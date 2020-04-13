e2646bf48591d1fa5216fb49aa3a2d69b7cd55f2
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-13 12:47:47 -0500

    marker appears at address on map test page

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,11 @@ require('dotenv').config()
 const googleMaps = require('@google/maps')
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY
 
-
-let map;
-
 function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 41.8781, lng: -87.6298},
-  zoom: 8
-});
+	const location = {lat: 41.932260, lng: -87.707900};
+	const map = new google.maps.Map(document.getElementById('map'), {
+		center: location,
+		zoom: 15
+	});
+	const marker = new google.maps.Marker({position: location, map: map})
 }
